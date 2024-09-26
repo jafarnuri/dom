ab339362d47d612a650bb89d933b2a646a965732
jafarnuri <jafarnuri1994@gmail.com>
2024-09-26 20:04:25 +0400

    add js and css files

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-window.onload = function () {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  let Bgcolor = document.getElementById("bgcolor");
+let element = document.querySelector(".element");
 
-  Bgcolor.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-};
-setInterval(
-  (window.onload = function () {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    let Bgcolor = document.getElementById("bgcolor");
+let colorArr = ["green", "yellow", "blue", "grey"];
 
-    Bgcolor.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-  }),
-  200
-);
+let i = 0;
+
+setInterval(() => {
+  if (i < colorArr.length) {
+    element.style.backgroundColor = colorArr[i];
+    i = i + 1;
+    i = i % colorArr.length;
+  }
+}, 2000);
+
+let telementbody = document.querySelector("tbody");
+
+let objets = [
+  {
+    name: "Jafar",
+    surname: "Nuriyev",
+  },
+
+  {
+    name: "Kenan",
+    surname: "Mahmudov",
+  },
+
+  {
+    name: "Allaverdi",
+    surname: "Hemidov",
+  },
+
+  {
+    name: "Gulcohre",
+    surname: "Qafarova",
+  },
+];
+let index = 0;
+let id = 1;
+
+setInterval(() => {
+  if (id <= objets.length) {
+    let trelemet = document.createElement("tr");
+    let tdelemet = document.createElement("td");
+    tdelemet.innerHTML = ` ${objets[index].name}`;
+    let tdelemet1 = document.createElement("td");
+    tdelemet1.innerHTML = ` ${objets[index].surname}`;
+    let tdelemet2 = document.createElement("td");
+    tdelemet2.innerHTML = `${id}`;
+    trelemet.append(tdelemet, tdelemet1, tdelemet2);
+    telementbody.append(trelemet);
+    id++;
+    index++;
+  }
+}, 2000);
